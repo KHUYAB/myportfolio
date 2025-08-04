7a196a16edadbe5e52f4c8cbe48a841e150d2549
KHUYAB <norhandkuyab@gmail.com>
2025-08-04 02:26:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -221,7 +221,7 @@ function closeCertificateModal() {
 
   document.addEventListener("DOMContentLoaded", function () {
     const aboutTarget = document.getElementById("about-typewriter-name");
-    const aboutText = "Nor kuyab";
+    const aboutText = "Norhan";
     let i = 0;
     let deleting = false;
 
@@ -252,3 +252,4 @@ function closeCertificateModal() {
 
     animateAbout();
   });
+
